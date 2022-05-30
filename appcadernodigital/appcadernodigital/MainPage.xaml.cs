2ad1ddb7391e6bc6b226e76fb3fb65bc2ec84c73
Usuario <Usuario@CPD02-M16>
2022-05-30 09:39:12 -0300

    Adicionar arquivos de projeto.

---
+++ appcadernodigital/appcadernodigital/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using appcadernodigital.Primeiro;
+using appcadernodigital.Segundo;
+using appcadernodigital.Terceiro;
+using System;
+using appcadernodigital;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appcadernodigital
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+ 
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DisciplinasPrimeiro());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DiciplinaSegundo());
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+           Navigation.PushAsync(new DisciplinaTerceiro());
+        }
+    }
+}
